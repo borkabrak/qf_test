cb91e5bfa4ed0aae47fdd955795ed21cdf66803a
Jonathan Carter <jcarter@str.com>
2014-01-07 16:33:39 -0600

    More robust contact form behavior attachment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 window.onload = function(){
-    document.querySelector("form#contact").onsubmit = function(event){
-        alert("Thanks for your input " + event.target.name.value + "!");
-        return false;
+
+    // Submit contact form
+    if (document.querySelector("form#contact") !== null){
+        document.querySelector("form#contact").onsubmit = function(event){
+            alert("Thanks for your input " + event.target.name.value + "!");
+            return false;
+        };
     };
+
 }
